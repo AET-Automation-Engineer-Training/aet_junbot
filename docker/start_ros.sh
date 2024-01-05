a3dc64e32ad43cd713a74f33adb529e0a5fa7897
lacie-life <00sao00ios00@gmail.com>
2024-01-05 20:10:57 +0900

    [Lacie] update docker config

--- docker/start_ros.sh
+++ docker/start_ros.sh
@@ -7,7 +7,7 @@ docker run -it --net=host \
     --env="QT_X11_NO_MITSHM=1" \
     --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
     --volume="/home/$USER/:/home/$USER/:rw" \
-    ros_noetic \
+    ros_noetic:05012024 \
     bash
 
 
