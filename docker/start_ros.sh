67dfcb2b008da44b4a216518cf2b3e170762bf56
Life Zero <55802547+lacie-life@users.noreply.github.com>
2024-01-21 18:09:55 +0900

    Update start_ros.sh

--- docker/start_ros.sh
+++ docker/start_ros.sh
@@ -2,7 +2,7 @@
 
 xhost +
 
-docker run -it --net=host \
+docker run -it --privileged  --net=host \
     --env="DISPLAY" \
     --env="QT_X11_NO_MITSHM=1" \
     --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
