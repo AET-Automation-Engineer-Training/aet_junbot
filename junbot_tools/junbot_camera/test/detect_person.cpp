f8a57644e7260d730190ebe739099fb369e840c6
AET <caohuynhat0209.com>
2024-03-06 23:26:52 +0700

    [sub sonar]

--- junbot_tools/junbot_camera/test/detect_person.cpp
+++ junbot_tools/junbot_camera/test/detect_person.cpp
@@ -3,6 +3,8 @@
 #include <vision_msgs/Detection2DArray.h>
 #include <vision_msgs/Detection2D.h>
 #include <iostream>
+#include <sstream>
+#include <string>
 #include <std_msgs/String.h>
 
 ros::Publisher pub_detection;
@@ -20,8 +22,19 @@ void detectionCallback(const vision_msgs::Detection2DArray &bbox_msgs) {
             pub_detection.publish(msg);
         }
     }
-    
-    
+}
+
+void sonarCallback(const std_msgs::String::ConstPtr &sonar_msgs) {
+    std::string tmp = sonar_msgs->data.c_str();
+    std::stringstream ss(tmp);
+    std::string distance[8];
+    for (int i = 0; i < 8; i++)
+    {
+        std::getline(ss, distance[i], ':');
+    }
+
+    ROS_INFO("distance: ");
+    std::cout << distance[0];
 }
 
 
@@ -30,6 +43,7 @@ int main(int argc, char **argv) {
     ros::init(argc, argv, "dectect_person");
     ros::NodeHandle n;
     ros::Subscriber sub_detection = n.subscribe("/detectnet/detections", 1000, detectionCallback);
+    ros::Subscriber sub_sonar = n.subscribe("/sonar_data", 1000, sonarCallback);
     pub_detection = n.advertise<std_msgs::String>("/warning_person", 1000); 
 
     while (ros::ok()) 
@@ -41,3 +55,4 @@ int main(int argc, char **argv) {
     }
     return 0;
 }
+
